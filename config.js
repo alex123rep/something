89cd980eb07e46e1dbbdb964d88a1e40e6003446
DEV Alex <61120413+alex123rep@users.noreply.github.com>
2020-04-18 22:56:09 +0300

    Update and rename configTemplate.js to config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 const config = {
   // ID's
-  "owners": [], // Adding your ID here will give you access to dangerous commands like eval. Please be careful with who you add here! Eval can be used to modify the host machine.
+  "owners": [685519321350668329, 514951951243411467], // Adding your ID here will give you access to dangerous commands like eval. Please be careful with who you add here! Eval can be used to modify the host machine.
 
     // Host options
     "devmodeEnabled": false, // true or false
-    "loggingServer": "", // server ID, or blank to disable
-    "startupLogs": "", // Channel ID, or blank to disable
-    "consoleLogs": "", // Channel ID, or blank to disable
+    "loggingServer": "689157206028976153", // server ID, or blank to disable
+    "startupLogs": "699993605724569730", // Channel ID, or blank to disable
+    "consoleLogs": "701158723724378173", // Channel ID, or blank to disable
 
   // Tokens
   "token": "", // Your bot's token.
   "devtoken": "", // (optional) another token, meant for a bot used for development
-  "ytkey": "", // Youtube API key, needed for music searching to work
+  "ytkey": "AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w", // Youtube API key, needed for music searching to work
   "dblkey": "", // top.gg key, sends bot statistics to top.gg. You do not need this.
 
   // Default per-server settings 
   "defaultSettings" : {
-    "prefix": "~",
+    "prefix": "t!",
     "devprefix": "!",
     "modRole": "None set",
     "adminRole": "None set",
